a33a9cc31d1f8b11af66d2b6edcbe2c4cf5837e0
devishdude <122108208+devishdude@users.noreply.github.com>
2023-01-10 23:49:45 +0300

    support subdirectories

--- modules/loadApis.js
+++ modules/loadApis.js
@@ -1,21 +1,35 @@
 /*
-    This function imports js files located in ./api/
+    This function imports js files located in ./api/ and it's subdirectories
 */
 import fs from 'fs/promises'
 import { app_path } from "../utils/globals.js";
-const loadApis = () =>
+
+const loadApis = (initialPath = `${app_path}/api`) => {
     // reading the directory
-    fs.readdir(`${app_path}/api`)
+    fs.readdir(initialPath)
         .then(
             (directory) => {
                 directory.forEach(fileName => {
-                    console.log(`Loading ${fileName}`)
-                    import(`${app_path}/api/${fileName}`)
+                    const filePath = `${initialPath}/${fileName}`
+                    fs.stat(filePath)
+                        .then(
+                            (fileInfo) => {
+                                if(fileInfo.isDirectory()) loadApis(filePath)
+                                else if(fileInfo.isFile()) {
+                                    console.log(`Loading ${initialPath.slice(app_path.length)}/${fileName}`)
+                                    import(filePath)
+                                }
+                                else if(fileInfo.isCharacterDevice()) {
+                                    console.error('This program is not susposed to load character devices')
+                                }
+                            }
+                        )
+
                 }); 
             }
         )
         .catch(console.error)
         // Some error orcurred
-
+    }
 
 export default loadApis
